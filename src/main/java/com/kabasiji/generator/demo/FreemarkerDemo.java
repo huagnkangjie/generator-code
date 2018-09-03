e9788d710f2d581705f2544cb5cf44622e655256
huangkangjie <1059976050@qq.com>
2018-09-03 23:10:22 +0800

    Initial commit

---
+++ src/main/java/com/kabasiji/generator/demo/FreemarkerDemo.java
@@ -0,0 +1,63 @@
+package com.kabasiji.generator.demo;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author huang_kangjie
+ * @create 2018-09-03 22:55
+ **/
+public class FreemarkerDemo {
+
+     private static final String TEMPLATE_PATH = "F:\\develop\\idea-workspace\\generator-code\\src\\main\\java\\com\\kabasiji\\generator\\templates";
+     //private static final String TEMPLATE_PATH = "com/kabasiji/generator/templates";
+     private static final String CLASS_PATH = "D://test";
+     //private static final String CLASS_PATH = "com/kabasiji/generator/demo";
+
+     public static void main(String[] args) {
+          // step1 创建freeMarker配置实例
+          Configuration configuration = new Configuration();
+          Writer out = null;
+          try {
+               // step2 获取模版路径
+               configuration.setDirectoryForTemplateLoading(new File(TEMPLATE_PATH));
+               // step3 创建数据模型
+               Map<String, Object> dataMap = new HashMap<String, Object>();
+               dataMap.put("classPath", "com.freemark.hello");
+               dataMap.put("className", "User");
+               dataMap.put("Id", "Id");
+               dataMap.put("userName", "userName");
+               dataMap.put("password","password");
+               // step4 加载模版文件
+               Template template = configuration.getTemplate("test.ftl");
+               // step5 生成数据
+               File docFile = new File(CLASS_PATH + "\\" + "User.java");
+               if(!docFile.exists()) {
+                    docFile.createNewFile();
+               }
+               out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(docFile)));
+               // step6 输出文件
+               template.process(dataMap, out);
+               System.out.println("^^^^^^^^^^^^^^^^^^^^^^^^User.java 文件创建成功 !");
+          } catch (Exception e) {
+               e.printStackTrace();
+          } finally {
+               try {
+                    if (null != out) {
+                         out.flush();
+                    }
+               } catch (Exception e2) {
+                    e2.printStackTrace();
+               }
+          }
+     }
+
+}
